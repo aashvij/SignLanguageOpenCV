4f36bbe95f3b53a8a277765e314eafba5b4b1456
Aashvi Jain <111461803+aashvij@users.noreply.github.com>
2024-06-02 00:17:32 +0200

    Add files via upload

--- cvScript.js
+++ cvScript.js
@@ -1,25 +1,46 @@
-console.log(cv.Mat);
-
-function openCvReady() {
-    console.log('spot 1');
-    cv['onRuntimeInitialized'] =()=>{
-        let canvasFrame = document.getElementById("canvasFrame");
-        let context = canvasFrame.getContext("2d");
-        let video = document.getElementById("videoElement");
-        let capture = new cv.VideoCapture(video);
-        let matrix = new cv.Mat(640, 480, cv.CV_8UC4);
-        const FPS = 30;
-
-        function processVideo(){
-            let start = Date.now();
-            capture.read(matrix);
-            context.drawImage(matrix, 0, 0, video.width, video.height);
-            // let imageData = context.getImageData(0, 0, video.width, video.height);
-            // matrix.data.set(imageData.data);
-            // cv.imshow("canvasFrame", matrix);
-            let delay = 1000 / FPS - (Date.now() - start);
-            setTimeout(processVideo, delay);
-        }
-        setTimeout(processVideo, 0);
+var Module = {
+    onRuntimeInitialized() {
+        console.log("spot1");
+        setupSignLangApp();
     }
-}
+  };
+  
+  function setupHtmlVideoCapture() {
+    // set up html video capture
+    let video = document.getElementById("videoElement");
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: false })
+      .then(function (stream) {
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch(function (err) {
+        console.log("An error occurred! " + err);
+      });
+    return video;
+  }
+  
+  function setupSignLangApp() {
+    const video = setupHtmlVideoCapture();
+    let videoCapture = new cv.VideoCapture(video);
+  
+    let mat = new cv.Mat(480, 640, cv.CV_8UC4);
+    let dst = new cv.Mat(480, 640, cv.CV_8UC4);
+  
+    const FPS = 30;
+  
+    function processVideo() {
+      let start = Date.now();
+  
+      // capture a frame from the video and put it into my opencv mat object for processing
+      videoCapture.read(mat);
+  
+      // do some processing on the mat object
+      cv.cvtColor(mat, dst, cv.COLOR_BGR2GRAY);
+      cv.imshow("canvasFrame", dst);
+  
+      let delay = 1000 / FPS - (Date.now() - start);
+      setTimeout(processVideo, delay);
+    }
+    setTimeout(processVideo, 0);
+  }
